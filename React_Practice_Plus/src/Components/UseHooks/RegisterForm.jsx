987f35c15996353e073d4f711cfb361273a5704d
SamirKhan21860 <msamirkhan561@gmail.com>
2024-07-03 06:34:26 +0500

    inputCompo, and RegisterForm files have been created learning coursera course React basics

---
+++ React_Practice_Plus/src/Components/UseHooks/RegisterForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export default function RegisterForm() {
+  const formData = {
+    firstName: "Luke",
+    lastName: "Jones",
+    email: "lukeJones@sculpture.com",
+  };
+
+  const [form, setForm] = useState(formData);
+
+  // const handleForm = (e) => {
+  //   ...formData,
+  //   [e.target.name]: e.target.value,
+  // }
+
+  return (
+    <>
+      <label>
+        First name:
+        <input
+          type="text"
+          value={form.firstName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              firstName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          type="text"
+          value={form.lastName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              lastName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={form.email}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              email: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <p>{`${form.firstName} ${form.lastName} (${form.email})`}</p>
+    </>
+  );
+}
--- React_Practice_Plus/src/main.jsx
+++ React_Practice_Plus/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import InputComponent from "./Components/UseHooks/InputComponent.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <InputComponent />
+  </React.StrictMode>
+);
